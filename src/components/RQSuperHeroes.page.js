0b2ed7d3734723816ec4f8674d61ad88c4378b86
hassanulinam <hassanulinam143@gmail.com>
2022-10-22 18:50:39 +0530

    useMutation to post/send data to server

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,10 +1,13 @@
 import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { useHeroesData } from "../hooks/useHeroesData";
+import { useAddHeroData, useHeroesData } from "../hooks/useHeroesData";
 
 const fetchSuperHeroes = () => axios.get("http://localhost:4000/superheroes");
 
 export const RQSuperHeroesPage = () => {
+  const [name, setName] = useState("");
+  const [alterEgo, setAlterEgo] = useState("");
   const { isLoading, data, isError, error, isFetching, refetch } =
     useHeroesData(fetchSuperHeroes);
   console.log({
@@ -12,19 +15,43 @@ export const RQSuperHeroesPage = () => {
     isFetching,
   });
 
+  const { mutate: addHero } = useAddHeroData();
+  const handleAddHeroClick = () => {
+    const heroData = { name, alterEgo };
+    addHero(heroData);
+  };
+
   if (isLoading) return <h2> Loading....</h2>;
-
   if (isError) return <h1>{error.message}</h1>;
-
   return (
     <>
       <h2>React Query Super Heroes Page</h2>
-      <button onClick={refetch}>Fetch Heroes</button>
-      {data?.data.map((hero) => (
-        <div key={hero.id}>
-          <Link to={`/rq-super-heroes/${hero.id}`}>{hero.name}</Link>
-        </div>
-      ))}
+      <div>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          value={alterEgo}
+          onChange={(e) => setAlterEgo(e.target.value)}
+        />
+        <button onClick={handleAddHeroClick}>Add Hero</button>
+      </div>
+      <button onClick={refetch}>Fetch heroes</button>
+      {data?.data.map((hero) => {
+        return (
+          <div key={hero.id}>
+            <Link to={`/rq-super-heroes/${hero.id}`}>
+              {hero.id} {hero.name}
+            </Link>
+          </div>
+        );
+      })}
+      {/* {data.map(heroName => {
+        return <div key={heroName}>{heroName}</div>
+      })} */}
     </>
   );
 };
--- src/hooks/useHeroesData.js
+++ src/hooks/useHeroesData.js
@@ -1,5 +1,14 @@
-import { useQuery } from "react-query";
+import axios from "axios";
+import { useMutation, useQuery } from "react-query";
 
 export const useHeroesData = (fetcher) => {
   return useQuery("super-heroes", fetcher);
+};
+
+const addHero = (heroData) => {
+  return axios.post("http://localhost:4000/superheroes", heroData);
+};
+
+export const useAddHeroData = () => {
+  return useMutation(addHero);
 };
